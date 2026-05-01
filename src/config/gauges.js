15d7dd878c2c145158805e5fc3c2221b09a954e4
Zane <168306286+NurseDan@users.noreply.github.com>
2026-04-30 19:10:54 -0500

    Add Guadalupe gauge configuration

---
+++ src/config/gauges.js
@@ -0,0 +1,65 @@
+export const GAUGES = [
+  {
+    id: '08165300',
+    name: 'North Fork Guadalupe River near Hunt',
+    shortName: 'North Fork near Hunt',
+    lat: 30.0696,
+    lng: -99.3378,
+    order: 1,
+    noaaSlug: null,
+    floodStageFt: null
+  },
+  {
+    id: '08165500',
+    name: 'Guadalupe River at Hunt',
+    shortName: 'Hunt',
+    lat: 30.0691,
+    lng: -99.3153,
+    order: 2,
+    noaaSlug: 'HNTT2',
+    floodStageFt: null
+  },
+  {
+    id: '08166140',
+    name: 'Guadalupe River above Bear Creek at Kerrville',
+    shortName: 'Above Kerrville',
+    lat: 30.0474,
+    lng: -99.1817,
+    order: 3,
+    noaaSlug: null,
+    floodStageFt: null
+  },
+  {
+    id: '08166200',
+    name: 'Guadalupe River at Kerrville',
+    shortName: 'Kerrville',
+    lat: 30.0463,
+    lng: -99.1403,
+    order: 4,
+    noaaSlug: 'KRVT2',
+    floodStageFt: null
+  },
+  {
+    id: '08166250',
+    name: 'Guadalupe River near Center Point',
+    shortName: 'Center Point',
+    lat: 29.9444,
+    lng: -99.0386,
+    order: 5,
+    noaaSlug: 'CNPT2',
+    floodStageFt: null
+  },
+  {
+    id: '08167000',
+    name: 'Guadalupe River at Comfort',
+    shortName: 'Comfort',
+    lat: 29.9666,
+    lng: -98.9070,
+    order: 6,
+    noaaSlug: 'CMFT2',
+    floodStageFt: null
+  }
+]
+
+export const REFRESH_MS = 60_000
+export const STALE_AFTER_MINUTES = 15
